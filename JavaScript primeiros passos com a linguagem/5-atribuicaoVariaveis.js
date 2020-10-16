483e27acee3e2d1303cd9a9b6ce215dec5f66c17
Renato Santos <renato_jfs@hotmail.com>
2020-10-15 23:40:23 -0300

    Variable atributes complete

--- JavaScript primeiros passos com a linguagem/5-atribuicaoVariaveis.js
+++ JavaScript primeiros passos com a linguagem/5-atribuicaoVariaveis.js
@@ -6,3 +6,9 @@ const sobrenome = "Bugan";
 //console.log(nome + " " + sobrenome)
 console.log(primeiroNome, sobrenome)
 console.log(`Meu nome é ${primeiroNome} ${sobrenome}`);
+
+let contador = 0
+contador = contador +1
+const nomeCompleto = primeiroNome + sobrenome;
+console.log(nomeCompleto);
+nomeCompleto =2;
