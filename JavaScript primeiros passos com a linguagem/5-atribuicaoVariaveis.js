1aa28325cb7144185de05a93128136dff4799009
Renato Santos <renato_jfs@hotmail.com>
2020-10-15 23:38:51 -0300

    Variable atributes

---
+++ JavaScript primeiros passos com a linguagem/5-atribuicaoVariaveis.js
@@ -0,0 +1,8 @@
+console.log("Trabalhando com atribuição de variáveis");
+const idade = 29;
+const primeiroNome = "Ricardo";
+const sobrenome = "Bugan";
+
+//console.log(nome + " " + sobrenome)
+console.log(primeiroNome, sobrenome)
+console.log(`Meu nome é ${primeiroNome} ${sobrenome}`);
