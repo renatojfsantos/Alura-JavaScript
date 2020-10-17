be6d08ca4e895659c2430e49c5382336a5ddf8c3
Renato Santos <renato_jfs@hotmail.com>
2020-10-16 22:55:42 -0300

    list for city

--- JavaScript primeiros passos com a linguagem/6-listas.js
+++ JavaScript primeiros passos com a linguagem/6-listas.js
@@ -10,3 +10,14 @@ const listaDeDestinos = new Array(
     `São Paulo`,
     `Rio de Janeiro`
 );
+
+listaDeDestinos.push(`Curitiba`) // adicionando um item na lista
+console.log("Destinos possíveis:");
+//console.log(salvador, saoPaulo, rioDeJaneiro)
+console.log(listaDeDestinos);
+
+listaDeDestinos.splice(1,1);
+console.log(listaDeDestinos);
+
+
+console.log(listaDeDestinos[1], listaDeDestinos[0]);
