2018df048199a24df31b8e89aff2b0c9ba3a0543
Renato Santos <renato_jfs@hotmail.com>
2020-10-16 22:55:01 -0300

    list

---
+++ JavaScript primeiros passos com a linguagem/6-listas.js
@@ -0,0 +1,12 @@
+console.log(`Trabalhando com listas`);
+// const salvador = `Salvador`;
+// const saoPaulo = `São Paulo`;
+// const rioDeJaneiro = `Rio de Janeiro`;
+
+let new = 2;
+
+const listaDeDestinos = new Array(
+    `Salvador`,
+    `São Paulo`,
+    `Rio de Janeiro`
+);
