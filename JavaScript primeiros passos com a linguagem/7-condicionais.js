e8296aacd5d7db50462a234c429e42c1cd281310
Renato Santos <renato_jfs@hotmail.com>
2020-10-18 00:06:40 -0300

    Conditional sequence function finish

--- JavaScript primeiros passos com a linguagem/7-condicionais.js
+++ JavaScript primeiros passos com a linguagem/7-condicionais.js
@@ -21,3 +21,25 @@ console.log(listaDeDestinos);
 // } else {
 //     console.log("Não é maior de Idade e não posso vender");
 // }
+
+if (idadeComprador >= 18 || estaAcompanhada == true) {
+  console.log("Boa Viagem!!");
+  listaDeDestinos.splice(2, 1); //removendo item
+} else {
+  console.log("Não é maior de Idade e não posso vender");
+}
+
+console.log("Embarque: \n\n")
+if(idadeComprador >= 18 && temPassagemComprada){
+  console.log("Boa viagem");
+}else{
+  console.log("Você não pode embarcar");
+}
+
+console.log(listaDeDestinos);
+
+// console.log(idadeComprador > 18);
+// console.log(idadeComprador < 18);
+// console.log(idadeComprador <= 18);
+// console.log(idadeComprador >= 18);
+// console.log(idadeComprador == 18);
