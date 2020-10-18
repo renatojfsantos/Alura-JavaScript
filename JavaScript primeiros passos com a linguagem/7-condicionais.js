c6a538fbe21eb44051d5645a1ba6d1c64914152c
Renato Santos <renato_jfs@hotmail.com>
2020-10-18 00:03:07 -0300

    Conditional sequence function

--- JavaScript primeiros passos com a linguagem/7-condicionais.js
+++ JavaScript primeiros passos com a linguagem/7-condicionais.js
@@ -8,3 +8,16 @@ const listaDeDestinos = new Array(
 const idadeComprador = 18;
 const estaAcompanhada = false;
 const temPassagemComprada = true;
+
+console.log("Destinos possíveis:");
+console.log(listaDeDestinos);
+
+// if (idadeComprador >= 18) {
+//     console.log("Comprador maior de idade");
+//     listaDeDestinos.splice(1, 1); //removendo item
+// } else if (estaAcompanhada == true) {
+//     console.log("Comprador está acompanhado");
+//     listaDeDestinos.splice(1, 1); //removendo item
+// } else {
+//     console.log("Não é maior de Idade e não posso vender");
+// }
