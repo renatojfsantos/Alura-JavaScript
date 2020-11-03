37f073ffa800dea409372936059a777f26ca248a
Renato Santos <renato_jfs@hotmail.com>
2020-11-03 06:50:29 -0300

    Clear index

--- JavaScript Programando a Orientação a Objetos/index.js
+++ JavaScript Programando a Orientação a Objetos/index.js
@@ -26,47 +26,48 @@
 // const cliente3Saldo = 0;
 // console.log(cliente1, cliente2);
 
-class Cliente{
-  nome;
-  cpf;
-}
+//class Cliente{
+//  nome;
+//  cpf;
+//}
 
-class ContaCorrente{
-  agencia;
-   // #saldo =0 https://github.com/tc39/proposal-class-fields#private-fields
-  _saldo = 0;
-  sacar(valor){
-    if(this._saldo >= valor){
-        this._saldo -= valor;
-        return valor;
-    }
-}
+//class ContaCorrente{
+//  agencia;
+//   // #saldo =0 https://github.com/tc39/proposal-class-fields#private-fields
+//  _saldo = 0;
+//  sacar(valor){
+//    if(this._saldo >= valor){
+//        this._saldo -= valor;
+//        return valor;
+//    }
+//}
 
-depositar(valor){
-    if(valor <= 0)
-    {
-        return;
-    } 
-    this._saldo += valor;           
-}
-}
-const cliente1 = new Cliente();
-cliente1.nome = "Ricardo";
-cliente1.cpf = 11122233309;
+//depositar(valor){
+//    if(valor <= 0)
+//    {
+//        return;
+//    } 
+//    this._saldo += valor;           
+//}
+//}
+//const cliente1 = new Cliente();
+//cliente1.nome = "Ricardo";
+//cliente1.cpf = 11122233309;
 
-const cliente2 = new Cliente();
-cliente2.nome = "Alice";
-cliente2.cpf = 88822233309;
+//const cliente2 = new Cliente();
+//cliente2.nome = "Alice";
+//cliente2.cpf = 88822233309;
 
 
-const contaCorrenteRicardo = new ContaCorrente();
-contaCorrenteRicardo.agencia = 1001;
+//const contaCorrenteRicardo = new ContaCorrente();
+//contaCorrenteRicardo.agencia = 1001;
 
-contaCorrenteRicardo.depositar(-100);
-contaCorrenteRicardo.depositar(100);
-contaCorrenteRicardo.depositar(100);
+//contaCorrenteRicardo.depositar(-100);
+//contaCorrenteRicardo.depositar(100);
+//contaCorrenteRicardo.depositar(100);
 
-const valorSacado = contaCorrenteRicardo.sacar(50);
-console.log(valorSacado);
+//const valorSacado = contaCorrenteRicardo.sacar(50);
+//console.log(valorSacado);
 
-console.log(contaCorrenteRicardo);
+//console.log(contaCorrenteRicardo);
+
