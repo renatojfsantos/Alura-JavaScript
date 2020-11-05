d87af5202d1b4cf94d8c36f55fecd4fc558ebbed
Renato Santos <renato_jfs@hotmail.com>
2020-11-04 21:50:04 -0300

    Complete current

--- JavaScript Programando a Orientação a Objetos/index.js
+++ JavaScript Programando a Orientação a Objetos/index.js
@@ -87,3 +87,13 @@ const contaCorrenteRicardo = new ContaCorrente();
 contaCorrenteRicardo.agencia = 1001;
 contaCorrenteRicardo.cliente = cliente1;
 contaCorrenteRicardo.depositar(500);
+
+const conta2 = new ContaCorrente();
+conta2.cliente = cliente1;
+conta2.agencia = 102;
+
+let valor = 200;
+contaCorrenteRicardo.tranferir(valor, conta2);
+
+conta2.saldo = 30000;
+console.log(conta2.saldo);
